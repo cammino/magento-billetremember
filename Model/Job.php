2c7364d6e85faceb342a8ceeb5600b115d763229
Marcelo Ferracioli <marceloferracioli@iMac-de-Marcelo.local>
2019-11-07 15:51:20 -0300

    Filter only new orders.

--- Model/Job.php
+++ Model/Job.php
@@ -36,6 +36,7 @@ class Cammino_Billetremember_Model_Job
         $hours = Mage::helper("billetremember")->getHours();
         
         $payments->getSelect()
+            ->where('state = \'new\' ')
             ->where('pagarme_boleto_url IS NOT NULL')
             ->where('DATE(pagarme_boleto_expiration_date) >= DATE(NOW())')
             ->where('DATE_ADD(pagarme_boleto_expiration_date, INTERVAL -'.$hours.' HOUR) < NOW()')
